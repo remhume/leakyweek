f7596d9d5acc4b32f50437f5ed96afe9341b5fd7
remmy hume <rem@remics.net>
2015-04-18 06:29:08 +0200

    Added title image

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,25 @@ PLAYGROUND.Transitions.prototype.postrender = function(){
 }
 
 playground({
-    
+    width: 900,
+    height: 600,
+    scale: 1,
+    smoothing: false,
+    create: function(){
+        this.loadImage("title");
+    },
+    ready: function(){
+        this.setState(LEAKYWEEK.title);
+    }
 });
+
+LEAKYWEEK.title = {
+    enter: function(){
+        
+    },
+    render: function(){
+        var title = this.app.images.title;
+        this.app.layer.clear('#333')
+            .drawImage(title, (this.app.width-title.width)/2, 0);
+    }
+};
