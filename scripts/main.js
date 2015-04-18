37b4916a4c09d9be7cade1e3ec2702f404ffaa39
remmy hume <rem@remics.net>
2015-04-18 04:28:48 +0200

    playground.js init

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var LEAKYWEEK = {};
+
+PLAYGROUND.Transitions.enabled = true;
+PLAYGROUND.Transitions.preferred = PLAYGROUND.Transitions.Split;
+PLAYGROUND.Transitions.prototype.postrender = function(){
+    if (this.progress >= 1) return;
+
+    if(PLAYGROUND.Transitions.enabled){
+        PLAYGROUND.Transitions.preferred(this, this.progress);
+    }
+}
+
+playground({
+    
+});
