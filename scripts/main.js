b23bd3fb893409efa3e6117075aa8032773fbb5e
kohlrabi <kohlrabi@aragonit>
2015-04-18 10:15:42 +0200

    scenario loading, prepared selection in title-screen

--- scripts/main.js
+++ scripts/main.js
@@ -17,11 +17,34 @@ playground({
     smoothing: false,
     create: function(){
         this.loadImage("title");
+
+        LEAKYWEEK.loadScenario("scenarios/test.json");
+        LEAKYWEEK.loadScenario("scenarios/example.json");
+
+        for(var k in LEAKYWEEK.SCENARIOS) {
+            this.loadImage(LEAKYWEEK.SCENARIOS[k].thumbnail);
+        }
     },
     ready: function(){
         this.setState(LEAKYWEEK.title);
     }
 });
+
+LEAKYWEEK.SCENARIOS = {};
+
+LEAKYWEEK.loadScenario = function(path) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === XMLHttpRequest.DONE) {
+            if(xhr.status === 200) {
+                var s = JSON.parse(xhr.responseText);
+                LEAKYWEEK.SCENARIOS[s.id] = s;
+            }
+        }
+    };
+    xhr.open('GET', path, false);
+    xhr.send();
+}
 
 LEAKYWEEK.days = {};
 
@@ -32,7 +55,21 @@ LEAKYWEEK.title = {
     render: function(){
         var title = this.app.images.title;
         this.app.layer.clear('#333')
-            .drawImage(title, (this.app.width-title.width)/2, 0);
+            .drawImage(title, (this.app.width-title.width)/2, 0)
+            .textAlign('left')
+            .fillStyle('#eee');
+
+        for(var i in LEAKYWEEK.SCENARIOS) {
+            var s = LEAKYWEEK.SCENARIOS[i];
+            this.app.layer
+                  .drawImage(this.app.images[s.thumbnail], (this.app.width-300)/2, 285+Object.keys(LEAKYWEEK.SCENARIOS).indexOf(i)*90)
+                  .font("15pt Monospace")
+                  .textAlign("left")
+                  .fillStyle("#eee")
+                  .fillText(s.title, (this.app.width-150)/2+60, 300+Object.keys(LEAKYWEEK.SCENARIOS).indexOf(i)*90)
+                  .font("12pt sans")
+                  .fillText(s.description, (this.app.width-150)/2+60, 330+Object.keys(LEAKYWEEK.SCENARIOS).indexOf(i)*90);
+        }
     }
 };
 
@@ -46,4 +83,4 @@ LEAKYWEEK.drawMap() = function(){
             .drawImage(tile.img, 0, 0)
             .restore();
     });
-});
+};
